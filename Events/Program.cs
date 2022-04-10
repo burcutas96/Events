545837738ab2b56a9384e39fc64a68244af2e63a
burcutas636@gmail.com <burcutas636@gmail.com>
2022-04-10 15:56:08 +0300

    Add project files.

---
+++ Events/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Product harDisk = new Product(50);
+            harDisk.ProductName = "Hard Disk";
+
+            Product gsm = new Product(50);
+            gsm.ProductName = "GSM";
+            gsm.StockControlevent += Gsm_StockControlevent; 
+
+            for (int i = 0; i < 10; i++)
+            {
+                harDisk.Sell(10);
+                gsm.Sell(10);
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+
+        private static void Gsm_StockControlevent()
+        {
+            Console.WriteLine("Gsm about to finish!!!");
+        }
+       
+    }
+    
+}
